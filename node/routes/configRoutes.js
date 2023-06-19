5d0eb7cb2d0f45c3e30ab81904535b68782b8368
Elisheva Einhoren <shevibr0@gmail.com>
2023-06-19 12:31:52 +0300

    added

--- node/routes/configRoutes.js
+++ node/routes/configRoutes.js
@@ -4,14 +4,13 @@ const companiesR = require("./companies");
 const devicesR = require("./devices");
 // const autosR = require("./autos");
 
-
 exports.routesInit = (app) => {
-  app.use("/",indexR);
-  app.use("/users",usersR);
-  app.use("/companies",companiesR);
-  app.use("/devices",devicesR);
+  app.use("/", indexR);
+  app.use("/users", usersR);
+  app.use("/companies", companiesR);
+  app.use("/devices", devicesR);
   // app.use("/autos",autosR);
-  app.use("*",(req,res) => {
-    res.status(404).json({msg:"Page/endpoint not found, 404"})
-  })
-}
+  app.use("*", (req, res) => {
+    res.status(404).json({ msg: "Page/endpoint not found, 404" });
+  });
+};
--- node/routes/devices.js
+++ node/routes/devices.js
@@ -4,48 +4,43 @@ const { auth } = require("../middlewares/auth");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-
   // Math.min -> מביא את המספר הקטן יותר מבין 2 המספרים של ה 20 ומספר העמוד
   let perPage = Math.min(req.query.perPage, 20) || 10;
-  let page = Number(req.query.page) || 1
+  let page = Number(req.query.page) || 1;
   let sort = req.query.sort || "_id";
   let reverse = req.query.reverse == "yes" ? 1 : -1;
 
   try {
-    let data = await DeviceModel
-      .find({})
+    let data = await DeviceModel.find({})
       .limit(perPage)
       .skip((page - 1) * perPage)
-      .sort({ [sort]: reverse })
+      .sort({ [sort]: reverse });
     res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
-  catch (err) {
-    console.log(err)
-    res.status(500).json(err)
-  }
-})
-router.get("/count",async (req, res) => {
+});
+router.get("/count", async (req, res) => {
   try {
-const perPage=req.query.perPage || 10
-const count=await DeviceModel.countDocuments({})
-res.json({count,pages:Math.ceil(count/perPage)});
+    const perPage = req.query.perPage || 10;
+    const count = await DeviceModel.countDocuments({});
+    res.json({ count, pages: Math.ceil(count / perPage) });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
-  catch (err) {
-    console.log(err)
-    res.status(500).json(err)
-  }
-})
+});
 router.get("/single/:id", async (req, res) => {
   try {
-    const id = req.params.id
+    const id = req.params.id;
     let data = await DeviceModel.findOne({ _id: id });
     res.json(data);
-  }
-  catch (err) {
+  } catch (err) {
     console.log(err);
-    res.status(502).json({ err })
+    res.status(502).json({ err });
   }
-})
+});
 
 router.post("/", auth, async (req, res) => {
   let validBody = validateDevice(req.body);
@@ -56,13 +51,12 @@ router.post("/", auth, async (req, res) => {
     let device = new DeviceModel(req.body);
     device.user_id = req.tokenData._id;
     await device.save();
-    res.json(device)
-  }
-  catch (err) {
+    res.json(device);
+  } catch (err) {
     console.log(err);
-    res.status(502).json({ err })
+    res.status(502).json({ err });
   }
-})
+});
 
 router.put("/:id", auth, async (req, res) => {
   let validBody = validateDevice(req.body);
@@ -73,35 +67,37 @@ router.put("/:id", auth, async (req, res) => {
     let id = req.params.id;
     let data;
     if (req.tokenData.role == "admin") {
-      data = await DeviceModel.updateOne({ _id: id }, req.body)
+      data = await DeviceModel.updateOne({ _id: id }, req.body);
+    } else {
+      data = await DeviceModel.updateOne(
+        { _id: id, user_id: req.tokenData._id },
+        req.body
+      );
     }
-    else {
-      data = await DeviceModel.updateOne({ _id: id, user_id: req.tokenData._id }, req.body)
-    }
-    res.json(data)
-  }
-  catch (err) {
+    res.json(data);
+  } catch (err) {
     console.log(err);
-    res.status(502).json({ err })
+    res.status(502).json({ err });
   }
-})
+});
 
 router.delete("/:id", auth, async (req, res) => {
   try {
     let id = req.params.id;
     let data;
     if (req.tokenData.role == "admin") {
-      data = await DeviceModel.deleteOne({ _id: id }, req.body)
+      data = await DeviceModel.deleteOne({ _id: id }, req.body);
+    } else {
+      data = await DeviceModel.deleteOne(
+        { _id: id, user_id: req.tokenData._id },
+        req.body
+      );
     }
-    else {
-      data = await DeviceModel.deleteOne({ _id: id, user_id: req.tokenData._id }, req.body)
-    }
-    res.json(data)
-  }
-  catch (err) {
+    res.json(data);
+  } catch (err) {
     console.log(err);
-    res.status(502).json({ err })
+    res.status(502).json({ err });
   }
-})
+});
 
 module.exports = router;
